0156d631bee08c6a021ec7fe6e5366454f121805
Daniel Mircea <daniel@viseztrance.com>
2020-03-08 13:29:03 +0000

    Highlight object names

--- simple-brightscript-mode.el
+++ simple-brightscript-mode.el
@@ -33,21 +33,67 @@
     (modify-syntax-entry ?\n ">" table)
     table))
 
-(defconst simple-brightscript-syntax-propertize-function ())
+(defconst simple-brightscript-reserved-keywords
+  '("and" "as" "dim" "each" "else" "end" "end for" "end function" "endif" "end sub"
+    "endwhile" "end if" "end while" "exit" "false" "for" "function" "goto" "if" "in"
+    "line_num" "m*" "next" "objfun" "or" "pos" "print" "rem" "return" "rnd" "step"
+    "stop" "sub" "tab" "then" "to" "true" "type" "while"))
+
+(defconst simple-brightscript-primitives
+  '("void" "dynamic" "boolean" "integer" "longinteger" "float" "double" "string"
+    "object" "interface"))
+
+(defconst simple-brightscript-global-functions
+  '("CopyFile" "CreateObject" "CreateDirectory" "DeleteFile" "DeleteDirectory"
+    "EjectDrive" "EnableZoneSupport" "EnableAudioMixer" "FormatDrive" "GetInterface"
+    "GetDefaultDrive" "LCase" "Left" "ListDir" "MatchFiles" "Mid" "MoveFile" "Pi"
+    "ReadAsciiFile" "RebootSystem" "Right" "RunGarbageCollector" "SetDefaultDrive"
+    "ShutdownSystem" "Sleep" "StringI" "String" "UCase" "UpTime" "Wait" "WriteAsciiFile"
+    "abs" "asc" "atn" "cdbl" "chr" "cint" "cos" "csng" "exp" "fix" "instr" "int"
+    "len" "log" "rnd" "sgn" "sgnI" "sin" "sqr" "str" "strI" "strtoi" "tan" "val"))
+
+(defconst simple-brightscript-objects
+  '("roArray" "roAssociativeArray" "roBoolean" "roByteArray" "roDouble"
+    "roIntrinsicDouble" "roFunction" "roGlobal" "roInt" "roFloat" "roString"
+    "roList" "roRegex" "roXMLElement" "roXMLList" "roAudioEventMx" "roAudioOutput"
+    "roAudioPlayer" "roAudioPlayerMx" "roCanvasWidget" "roClockWidget" "roHtmlWidget"
+    "roImageBuffer" "roImagePlayer" "roImageWidget" "roRectangle" "roShoutcastStream"
+    "roShoutcastStreamEvent" "roIRRemotePress" "roTextField" "roTextWidget"
+    "roVideoEvent" "roAudioEvent" "roVideoInput" "roVideoMode" "roVideoPlayer"
+    "roSqliteStatement" "roTouchCalibrationEvent" "roTouchEvent" "roTouchScreen"
+    "roBrightPackage" "roHashGenerator" "roReadFile" "roCreateFile" "roReadWriteFile"
+    "roAppendFile" "roRegistry" "roRegistrySection" "roSqliteDatabase" "roSqliteEvent"
+    "roStorageAttached" "roStorageDetached" "roStorageHotplug" "roStorageInfo"
+    "roAssetCollection" "roAssetFetcher" "roAssetFetcherEvent"
+    "roAssetFetcherProgressEvent" "roAssetPool" "roAssetPoolFiles" "roAssetRealizer"
+    "roAssetRealizerEvent" "roDatagramSender" "roDatagramReceiver" "roDatagramSocket"
+    "roDatagramEvent" "roHttpEvent" "roHttpServer" "roMimeStream" "roMimeStreamEvent"
+    "roNetworkAdvertisement" "roNetworkAttached" "roNetworkDetached"
+    "roNetworkConfiguration" "roNetworkHotplug" "roRssParser" "roRssArticle"
+    "roRtspStream" "roRtspStreamEvent" "roShoutcastStream" "roShoutcastStreamEvent"
+    "roSnmpAgent" "roSnmpEvent" "roStreamByteEvent" "roStreamConnectResultEvent"
+    "roStreamEndEvent" "roStreamLineEvent" "roSyncSpec" "roTCPConnectEvent"
+    "roTCPServer" "roTCPStream" "roUrlEvent" "roUrlStream" "roUrlTransfer"
+    "roCecInterface" "roCecRxFrameEvent" "roCecTxCompleteEvent" "roChannelManager"
+    "roControlPort" "roControlUp" "roControlDown" "roGpioControlPort" "roGpioButton"
+    "roIRRemote" "roKeyboard" "roKeyboardPress" "roMessagePort" "roSequenceMatcher"
+    "roSequenceMatchEvent" "roSerialPort" "roDeviceInfo" "roResourceManager"
+    "roSystemLog" "roDateTime" "roSystemTime" "roTimer" "roTimerEvent" "roTimeSpan"
+    "roSyncPool" "roSyncPoolEvent" "roSyncPoolFiles" "roSyncPoolProgressEvent"))
+
+(defface simple-brightscript-highlight-face
+  '((t :weight bold))
+  "Face for highlighting substrings in a text"
+  :group 'simple-brightscript-mode)
 
 (defvar simple-brightscript-font-lock
-  (let*
-      ((reserved-keywords '("and" "dim" "each" "else" "end" "end for" "end function" "endif" "end sub" "endwhile" "end if" "end while" "exit" "false" "for" "function" "if" "as" "in" "line_num" "m*" "next" "objfun" "or" "pos" "print" "rem" "return" "rnd" "step" "stop" "sub" "tab" "then" "to" "true" "type" "while" "goto"))
-       (primitives '("void" "dynamic" "boolean" "integer" "longinteger" "float" "double" "string" "object" "interface"))
-       (object-names '("roDeviceInfo" "roMessagePort"))
-       (global-functions '("CreateObject" "Sleep" "asc" "chr" "len" "str" "strI" "val" "abs" "atn" "cdbl" "cint" "cos" "exp" "fix" "int" "log" "sgn" "sgnI" "sin" "sqr" "tan" "Left" "Right" "StringI" "String" "Mid" "instr" "GetInterface" "Wait" "ReadAsciiFile" "WriteAsciiFile" "ListDir" "MatchFiles" "LCase" "UCase" "DeleteFile" "DeleteDirectory" "CreateDirectory" "RebootSystem" "ShutdownSystem" "UpTime" "csng" "FormatDrive" "EjectDrive" "CopyFile" "MoveFile" "strtoi" "rnd" "RunGarbageCollector" "GetDefaultDrive" "SetDefaultDrive" "EnableZoneSupport" "EnableAudioMixer" "Pi"))
-       (reserved-keywords-regexp (regexp-opt reserved-keywords 'words))
-       (primitives-regexp (regexp-opt primitives 'words))
-       (object-names-regexp (regexp-opt object-names 'words))
-       (global-functions-regexp (regexp-opt global-functions 'words)))
-
+  (let
+      ((reserved-keywords-regexp (regexp-opt simple-brightscript-reserved-keywords 'words))
+       (primitives-regexp (regexp-opt simple-brightscript-primitives 'words))
+       (global-functions-regexp (regexp-opt simple-brightscript-global-functions 'words))
+       (objects-regexp (regexp-opt simple-brightscript-objects 'words)))
     `(
-      ("^[[:space:]]*?\\(?:sub\\|function\\)\\(?:[[:space:]]*\\)?\\([[:word:]]+\\)?\\(?:[^( \t\n]\\)?"
+      ("^[[:space:]]*?\\(?:sub\\|function\\)\\(?:[[:space:]]*\\)?\\([[:word:]]+\\)\\(?:[^( \t\n]\\)?"
        (1 font-lock-function-name-face)
        ("\\([[:word:]]+\\).*?\\(?:,\\|)\\)"
         (save-excursion
@@ -62,8 +108,9 @@
       ("\\(==\\|invalid\\|\\(?:[[:word:]]*:\\)\\)" 1 font-lock-warning-face)
       ("goto\\(?:[[:space:]]*\\)?\\([[:word:]]*\\)" 1 font-lock-warning-face)
       ("\\_<\\(m\\)\\." 1 font-lock-variable-name-face)
+      ("^REM\\(.*\\)?$" . font-lock-comment-face)
+      (,objects-regexp 0 'simple-brightscript-highlight-face append)
       (,primitives-regexp . font-lock-type-face)
-      (,object-names-regexp . font-lock-type-face)
       (,reserved-keywords-regexp . font-lock-builtin-face)
       (,global-functions-regexp . font-lock-constant-face))))
 
@@ -74,9 +121,7 @@
 
   (setq-local comment-start "'")
   (setq-local comment-end "")
-  (set-syntax-table simple-brightscript-mode-syntax-table)
-  (setq-local syntax-propertize-function simple-brightscript-syntax-propertize-function)
-  )
+  (set-syntax-table simple-brightscript-mode-syntax-table))
 
 (defun case-insensitive-advice ()
   (setq-local font-lock-keywords-case-fold-search t))
